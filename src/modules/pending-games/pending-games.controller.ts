f8e637601748692ca9c7d93be2f8f0efe8ce9cd5
bruno <bruno.li@intelbras.com.br>
2023-04-28 17:37:11 -0300

    feat: implement pending games module

--- src/modules/pending-games/pending-games.controller.ts
+++ src/modules/pending-games/pending-games.controller.ts
@@ -6,40 +6,80 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Request,
+  Query,
+  HttpCode,
 } from '@nestjs/common';
 import { PendingGamesService } from './pending-games.service';
-import { CreatePendingGameDto } from './dto/create-pending-game.dto';
-import { UpdatePendingGameDto } from './dto/update-pending-game.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { AddPendingGameDTO } from './dto/add-pending-game.dto';
+import { SearchGameDTO } from '../games/dto/search-game.dto';
+import { UpdatePendingGameDTO } from './dto/update-pending-game.dto';
+import { AddPlayingGameDTO } from '../playing-games/dto/add-playing-game.dto';
 
 @Controller('pending-games')
+@ApiTags('pending-games')
+@ApiBearerAuth()
 export class PendingGamesController {
   constructor(private readonly pendingGamesService: PendingGamesService) {}
 
-  @Post()
-  create(@Body() createPendingGameDto: CreatePendingGameDto) {
-    return this.pendingGamesService.create(createPendingGameDto);
+  @UseGuards(JwtAuthGuard)
+  @Post('add')
+  async add(@Request() req, @Body() addPendingGameDTO: AddPendingGameDTO) {
+    return this.pendingGamesService.add(req.user.id, addPendingGameDTO);
   }
 
-  @Get()
-  findAll() {
-    return this.pendingGamesService.findAll();
+  @UseGuards(JwtAuthGuard)
+  @Get('list')
+  async list(@Request() req, @Query() searchGameDTO: SearchGameDTO) {
+    return await this.pendingGamesService.list(
+      req.user.id,
+      searchGameDTO.name,
+      searchGameDTO.page,
+      searchGameDTO.limit,
+    );
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.pendingGamesService.findOne(+id);
+  @UseGuards(JwtAuthGuard)
+  @Get('find/:id')
+  async find(@Request() req, @Param('id') id: number) {
+    return await this.pendingGamesService.find(id, req.user.id);
   }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updatePendingGameDto: UpdatePendingGameDto,
+  @UseGuards(JwtAuthGuard)
+  @Patch('edit/:id')
+  async update(
+    @Request() req,
+    @Param('id') id: number,
+    @Body() updatePendingGameDTO: UpdatePendingGameDTO,
   ) {
-    return this.pendingGamesService.update(+id, updatePendingGameDto);
+    return await this.pendingGamesService.update(
+      id,
+      req.user.id,
+      updatePendingGameDTO,
+    );
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.pendingGamesService.remove(+id);
+  @UseGuards(JwtAuthGuard)
+  @Delete('remove/:id')
+  @HttpCode(204)
+  async delete(@Request() req, @Param('id') id: number) {
+    return await this.pendingGamesService.delete(id, req.user.id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post(':id/upgrade-to-playing')
+  async upgradeToPlaying(
+    @Request() req,
+    @Param('id') id: number,
+    @Body() addPlayingGameDTO: AddPlayingGameDTO,
+  ) {
+    return await this.pendingGamesService.upgradeToPlaying(
+      id,
+      req.user.id,
+      addPlayingGameDTO,
+    );
   }
 }
